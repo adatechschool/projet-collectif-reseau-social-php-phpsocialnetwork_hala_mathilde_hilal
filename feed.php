adce695f9a87b8d45047993874042e1cd0803801
HilalLahreche <hilallahreche@gmail.com>
2024-05-30 18:28:53 +0200

    modif image logo

--- feed.php
+++ feed.php
@@ -9,7 +9,7 @@
     </head>
     <body>
         <header>
-        <a href='admin.php'><img src="resoc.jpg" alt="Logo de notre réseau social"/></a>
+        <a href='admin.php'><img src="resoc.jpg" alt="Logo de notre réseau social"/>
             <nav id="menu">
                 <a href="news.php">Actualités</a>
                 <a href="wall.php?user_id=5">Mur</a>
---
+++ linkUser.php
@@ -0,0 +1,10 @@
+<a href="wall.php?user_id=<?php echo htmlspecialchars($users['id'], ENT_QUOTES, 'UTF-8'); ?>">
+    <?php echo htmlspecialchars($users['alias'], ENT_QUOTES, 'UTF-8'); ?>
+</a>
+<!--
+ou
+
+<a href="wall.php?user_id=<?php  echo $users['id']?>"><?php echo htmlspecialchars($users['alias']); ?></a> -->
+
+
+
